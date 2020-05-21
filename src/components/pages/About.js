833117299f5559034a80ba83d44c0f0843438c44
Yimeng Yu <yimeng.yu@gmail.com>
2020-05-20 23:44:59 -0700

    update about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,10 +4,9 @@ class About extends Component {
     render() {
         return (
             <div>
-                <h1>Find the Recipe</h1>
+                <h2>Find the Recipe</h2>
                 <p>Find the recipe to help you build your dishes</p>
-                <p> About me: </p>
-                <h4>I am a person who loves to cook and enjoying to create amazing dishes</h4>
+                <p> Know the cooking time, know the ingredients, time to prepare your meals!</p>
                 <p>Version: 1.0.0</p>
             </div>
         )
